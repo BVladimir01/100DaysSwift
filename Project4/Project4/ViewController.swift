e3734bd6781aed9ebbf3c90411d5520828edb2ba
Vladimir <bizin.vladimir01@gmail.com>
2024-12-10 09:04:30 +0300

    complete tasks 1,2 of challenge26

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
     var progressView: UIProgressView!
-    private(set) var websites = ["Apple", "HackingWithSwift"]
+    private(set) var websites = ["Apple", "HackingWithSwift", "Google"]
+    private var leftArrow: UIBarButtonItem!
+    private var rightArrow: UIBarButtonItem!
     
     override func loadView() {
         webView = WKWebView()
@@ -26,24 +28,36 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
         title = "WebView"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-        
+        leftArrow = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(goBack))
+        leftArrow.isEnabled = webView.canGoBack
+        rightArrow = UIBarButtonItem(image: UIImage(systemName: "chevron.forward"), style: .plain, target: self, action: #selector(goForward))
+        rightArrow.isEnabled = webView.canGoForward
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
         let progressButton = UIBarButtonItem(customView: progressView)
         
-        toolbarItems = [progressButton, spacer, refresh]
+        toolbarItems = [leftArrow, spacer, rightArrow, spacer, progressButton, spacer, refresh]
         navigationController?.isToolbarHidden = false
         
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack), options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward), options: .new, context: nil)
     }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == "estimatedProgress" {
+        switch keyPath {
+        case "estimatedProgress":
             progressView.progress = Float(webView.estimatedProgress)
+        case "canGoBack":
+            leftArrow.isEnabled = webView.canGoBack
+        case "canGoForward":
+            rightArrow.isEnabled = webView.canGoForward
+        default:
+            break
         }
     }
 
@@ -67,17 +81,44 @@ class ViewController: UIViewController, WKNavigationDelegate {
     }
     
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let ac = UIAlertController(title: "Restricted website", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
         let url = navigationAction.request.url
         if let host = url?.host() {
-            print(host)
             for website in websites {
                 if host.contains(website.lowercased()) {
                     decisionHandler(.allow)
                     return
                 }
             }
+            present(ac, animated: true)
         }
         decisionHandler(.cancel)
+    }
+    
+    @objc
+    private func goBack() {
+        webView.goBack()
+        if let host = webView.url?.host() {
+            print(host)
+            for website in websites {
+                if host.contains(website.lowercased()) {
+                    navigationItem.title = website
+                }
+            }
+        }
+    }
+    
+    @objc
+    private func goForward() {
+        webView.goForward()
+        if let host = webView.url?.host() {
+            for website in websites {
+                if host.contains(website.lowercased()) {
+                    navigationItem.title = website
+                }
+            }
+        }
     }
 }
 

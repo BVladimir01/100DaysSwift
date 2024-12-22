7cb2f9e05f799734a88dac2f71e86c3909877f6c
Vladimir <bizin.vladimir01@gmail.com>
2024-12-22 21:12:47 +0300

    day 42,43,44: add project10, uiCollectionView project, complete challenges for it. does not work as inteded. seems like cell view is clipped

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Vladimir on 21.12.2024.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var imageName: String
+    
+    init(name: String, imageName: String) {
+        self.name = name
+        self.imageName = imageName
+    }
+    
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Vladimir on 21.12.2024.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
